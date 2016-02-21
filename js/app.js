85bc95d2751ee59b3143316b7a6c5bff393a8c79
daffourtit <daniel.affourtit@blackbaud.com>
2016-02-21 14:47:49 -0500

    updated animation

--- js/app.js
+++ js/app.js
@@ -10,10 +10,19 @@ $(document).ready(function() {
         $('.ryu-still').show();
     })
     .mousedown(function() {
+        playHadouken();
         $('.ryu-still').hide();
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
-        $('.hadouken').show();
+        $('.hadouken').finish().show()
+            .animate(
+            {'left': '1020px'},
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '520px');
+            }
+            );
     })
     .mouseup(function() {
         $('.ryu-throwing').hide();
@@ -21,3 +30,9 @@ $(document).ready(function() {
         $('.ryu-ready').show();
     });
 });
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
